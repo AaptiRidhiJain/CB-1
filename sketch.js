1a18cac10891d2c0d88617d12d3b28e546493758
AaptiRidhiJain <72203592+AaptiRidhiJain@users.noreply.github.com>
2020-11-27 14:29:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var paper;
+var ground;
+var r;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+  Engine.run(engine);
+  
+  paper = new Paper(700,240,300,240); 
+  ground = new Ground(700,700,700,700);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  paper.display();
+  ground.display();
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+  if(keyCode === UP_ARROW){
+    Matter.Body.applyForce(paperObject.body,paperObect.body,position,{x:85,y:-85});
+  }
+}
+
